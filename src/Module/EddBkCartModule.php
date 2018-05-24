0d4c51a1cbc9a378815005cf563f763325a27a9f
Miguel Muscat <miguelmuscat93@gmail.com>
2018-05-24 14:00:15 +0200

    Added module files and module class

---
+++ src/Module/EddBkCartModule.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace RebelCode\EddBookings\Cart\Module;
+
+use Dhii\Config\ConfigFactoryInterface;
+use Dhii\Data\Container\ContainerFactoryInterface;
+use Dhii\Event\EventFactoryInterface;
+use Dhii\Exception\InternalException;
+use Dhii\Util\String\StringableInterface as Stringable;
+use Psr\Container\ContainerInterface;
+use Psr\EventManager\EventManagerInterface;
+use RebelCode\Modular\Module\AbstractBaseModule;
+
+/**
+ * The EDD Bookings cart module class.
+ *
+ * @since [*next-version*]
+ */
+class EddBkCartModule extends AbstractBaseModule
+{
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable         $key                  The module key.
+     * @param string[]|Stringable[]     $dependencies         The module dependencies.
+     * @param ConfigFactoryInterface    $configFactory        The config factory.
+     * @param ContainerFactoryInterface $containerFactory     The container factory.
+     * @param ContainerFactoryInterface $compContainerFactory The composite container factory.
+     * @param EventManagerInterface     $eventManager         The event manager.
+     * @param EventFactoryInterface     $eventFactory         The event factory.
+     */
+    public function __construct(
+        $key,
+        $dependencies,
+        $configFactory,
+        $containerFactory,
+        $compContainerFactory,
+        $eventManager,
+        $eventFactory
+    ) {
+        $this->_initModule($key, $dependencies, $configFactory, $containerFactory, $compContainerFactory);
+        $this->_initModuleEvents($eventManager, $eventFactory);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     *
+     * @throws InternalException If an error occurred while reading the config or services files.
+     */
+    public function setup()
+    {
+        return $this->_setupContainer(
+            $this->_loadPhpConfigFile(EDDBK_CART_MODULE_CONFIG_FILE),
+            $this->_loadPhpConfigFile(EDDBK_CART_MODULE_SERVICES_FILE)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function run(ContainerInterface $c = null)
+    {
+        if ($c === null) {
+            return;
+        }
+    }
+}
