4653d0ab599dce6e2bc4f96038b0d44794bbb40f
Miguel Muscat <miguelmuscat93@gmail.com>
2018-05-24 14:56:14 +0200

    Added EDD cart service

--- services.php
+++ services.php
@@ -1,3 +1,15 @@
 <?php
 
-return [];
+use Psr\Container\ContainerInterface;
+use RebelCode\EddBookings\Cart\Module\AddBookingToCartHandler;
+
+return [
+    /*
+     * The EDD cart instance.
+     *
+     * @since [*next-version*]
+     */
+    'edd_cart' => function (ContainerInterface $c) {
+        return EDD()->cart;
+    },
+];
