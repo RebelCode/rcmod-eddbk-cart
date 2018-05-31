a7e5d1a837a1bd635af1db71942452b5392a3239
Miguel Muscat <miguelmuscat93@gmail.com>
2018-05-31 18:56:06 +0200

    Added `BookingValueAwareFactory`

---
+++ src/BookingValueAwareFactory.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace RebelCode\EddBookings\Cart;
+
+use Dhii\Data\Container\ContainerGetCapableTrait;
+use Dhii\Data\Container\ContainerHasCapableTrait;
+use Dhii\Data\Container\CreateContainerExceptionCapableTrait;
+use Dhii\Data\Container\CreateNotFoundExceptionCapableTrait;
+use Dhii\Data\Container\NormalizeKeyCapableTrait;
+use Dhii\Data\ValueAwareInterface;
+use Dhii\Exception\CreateInvalidArgumentExceptionCapableTrait;
+use Dhii\Exception\CreateOutOfRangeExceptionCapableTrait;
+use Dhii\Factory\Exception\CreateCouldNotMakeExceptionCapableTrait;
+use Dhii\Factory\FactoryInterface;
+use Dhii\I18n\StringTranslatingTrait;
+use Dhii\Util\Normalization\NormalizeStringCapableTrait;
+
+/**
+ * The factory for creating booking value-aware objects.
+ *
+ * @since [*next-version*]
+ */
+class BookingValueAwareFactory implements FactoryInterface
+{
+    /* @since [*next-version*] */
+    use ContainerGetCapableTrait;
+
+    /* @since [*next-version*] */
+    use ContainerHasCapableTrait;
+
+    /* @since [*next-version*] */
+    use NormalizeKeyCapableTrait;
+
+    /* @since [*next-version*] */
+    use NormalizeStringCapableTrait;
+
+    /* @since [*next-version*] */
+    use CreateContainerExceptionCapableTrait;
+
+    /* @since [*next-version*] */
+    use CreateNotFoundExceptionCapableTrait;
+
+    /* @since [*next-version*] */
+    use CreateCouldNotMakeExceptionCapableTrait;
+
+    /* @since [*next-version*] */
+    use CreateOutOfRangeExceptionCapableTrait;
+
+    /* @since [*next-version*] */
+    use CreateInvalidArgumentExceptionCapableTrait;
+
+    /* @since [*next-version*] */
+    use StringTranslatingTrait;
+
+    /**
+     * The key in the config where the booking is retrieved from.
+     *
+     * @since [*next-version*]
+     */
+    const K_BOOKING = 'booking';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     *
+     * @return ValueAwareInterface The created booking value aware object.
+     */
+    public function make($config = null)
+    {
+        if ($config == null || !$this->_containerHas($config, static::K_BOOKING)) {
+            throw $this->_createCouldNotMakeException(
+                $this->__('Missing booking in factory config'), null, null, $config
+            );
+        }
+
+        $booking = $this->_containerGet($config, static::K_BOOKING);
+
+        return new BookingValueAware($booking);
+    }
+}
